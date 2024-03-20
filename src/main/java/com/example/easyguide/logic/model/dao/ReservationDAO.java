1b5ef4455f153051b689b11544e349f807811171
NicolaViolante <129511509+NicolaViolante@users.noreply.github.com>
2024-03-20 14:20:06 +0100

    code smells

--- src/main/java/com/example/easyguide/logic/model/dao/ReservationDAO.java
+++ src/main/java/com/example/easyguide/logic/model/dao/ReservationDAO.java
@@ -127,6 +127,7 @@ public class ReservationDAO {
         }
         String sql1 = "SELECT " + PEOPLE + "," +  PRICE + " FROM reservation WHERE " + TOURISTMAIL + " = ?" +AND + GUIDEMAIL + " = ?" +AND
                 + DATE + " = ?" +AND + TIME + " = ?"  + AND + TOURNAME + " = ?";
+        try{
         stmt = conn.prepareStatement(sql1, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         stmt.setString(1, reservation.getTouristMail());
         stmt.setString(2, reservation.getGuideMail());
@@ -140,7 +141,11 @@ public class ReservationDAO {
             reservation.setPrice(rs.getFloat(2));
         }
         rs.close();
-        stmt.close();
+        }
+        finally {
+            stmt.close();
+        }
+
 
     }
 }
