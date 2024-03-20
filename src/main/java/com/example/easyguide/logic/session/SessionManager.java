b2d63363b17185199fef9f44a65887afdc116cac
NicolaViolante <129511509+NicolaViolante@users.noreply.github.com>
2024-03-20 14:43:09 +0100

    singleton code smell

--- src/main/java/com/example/easyguide/logic/session/SessionManager.java
+++ src/main/java/com/example/easyguide/logic/session/SessionManager.java
@@ -4,11 +4,13 @@ import com.example.easyguide.logic.model.domain.User;
 
 
 public class SessionManager {
-    private static SessionManager instance ;
+    private static SessionManager instance;
 
     protected User currentUser;
     public User getCurrentUser() {return currentUser;}
-
+    private SessionManager() {
+        currentUser = null;
+    }
 
     public static synchronized SessionManager getInstance(){
         if(SessionManager.instance == null)
