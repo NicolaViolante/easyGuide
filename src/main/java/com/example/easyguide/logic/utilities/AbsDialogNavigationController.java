f22b2afb978cf7da2a6a443a2ec5a9e79ac7cd88
NicolaViolante <129511509+NicolaViolante@users.noreply.github.com>
2024-03-23 19:23:59 +0100

    AbsDialogNavigationController (da rivedere)

---
+++ src/main/java/com/example/easyguide/logic/utilities/AbsDialogNavigationController.java
@@ -0,0 +1,49 @@
+package com.example.easyguide.logic.utilities;
+
+import javafx.scene.control.Alert;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AbsDialogNavigationController {
+    protected Alert errorAlert;
+    protected Alert infoAlert;
+    protected Alert confirmationAlert;
+
+    @FXML
+    public void initialize(){
+        errorAlert = new Alert(Alert.AlertType.ERROR);
+        infoAlert = new Alert(Alert.AlertType.INFORMATION);
+        confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
+    }
+
+    protected void showInfoAlert(String title, String header, String content){
+        infoAlert.setTitle(title);
+        infoAlert.setHeaderText(header);
+        infoAlert.setContentText(content);
+        infoAlert.show();
+    }
+    protected void showErrorAlert(String title, String header, String content){
+        errorAlert.setTitle(title);
+        errorAlert.setHeaderText(header);
+        errorAlert.setContentText(content);
+        errorAlert.show();
+    }
+    protected void showConfirmationAlert(String title, String header, String content){
+        confirmationAlert.setTitle(title);
+        confirmationAlert.setHeaderText(header);
+        confirmationAlert.setContentText(content);
+        confirmationAlert.show();
+    }
+
+    protected void goToPage(String page){
+        try{
+            NavigatorSingleton.getInstance().goToPage(page);
+        }
+        catch (IOException e){
+            Logger.getAnonymousLogger().log(Level.INFO, e.getMessage());
+        }
+    }
+}
