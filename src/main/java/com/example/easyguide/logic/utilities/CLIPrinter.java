76edeecee094f2138d57896b50fad9a0cf61becb
NicolaViolante <129511509+NicolaViolante@users.noreply.github.com>
2024-05-02 20:23:59 +0200

    cambiati dettagli finali

--- src/main/java/com/example/easyguide/logic/utilities/CLIPrinter.java
+++ src/main/java/com/example/easyguide/logic/utilities/CLIPrinter.java
@@ -6,18 +6,18 @@ public class CLIPrinter {
     private CLIPrinter() { throw new IllegalStateException("Utility class");}
     public static void printMessage(String s) { System.out.print(s);}
     public static void newLine() {System.out.printf("%n");}
-    public static void printListOfAvailableTours(int i, String name, float price) {System.out.printf("%s) Tour: %s, price %s%n",i , name, price);}
+    public static void printListOfAvailableTours(int i, String name, float price) {System.out.printf("%s) Tour: %s, price %s€%n",i , name, price);}
     public static void printListOfAvailableDates(int i, java.sql.Date date) {System.out.printf("%s)  %s%n",i , date);}
     public static void printListOfAvailableTimes(int i, java.sql.Time time) {System.out.printf("%s)  %s%n",i , time);}
     public static void printNumbers(int i) {System.out.printf("%s) ", i);}
     public static void printReservation(int i, String tourName, float price, java.sql.Date date, java.sql.Time time,
                                         int people, String touristMail)
-                                            {System.out.printf("%s) %s; price %s; date %s; time %s; people %s; touristMail %s.%n",
+                                            {System.out.printf("%s) %s; price %s€; date %s; time %s; people %s; touristMail %s.%n",
                                                     i, tourName, price, date, time, people, touristMail);
                                             }
     public static void printRequest(String tourName, float price, java.sql.Date date, java.sql.Time time,
                                         int people, String guideMail, Status status)
-    {System.out.printf("%s; price %s; date %s; time %s; people %s; guideMail %s; status %s.%n",
+    {System.out.printf("%s; price %s€; date %s; time %s; people %s; guideMail %s; status %s.%n",
              tourName, price, date, time, people, guideMail, status);
     }
 }
